7937473ce4ffec284100ada6e9a7986d87ff1515
GustavoCampos <148006984+GustavoH-C@users.noreply.github.com>
2023-10-18 21:20:14 -0300

    Update Robo.java

--- src/entidades/Robo.java
+++ src/entidades/Robo.java
@@ -2,16 +2,27 @@ package entidades;
 
 public class Robo {
 	
+	private String nome;
 	private Celula posicao;
 	private int tempoSegundos;
 	
-	public Robo() {}
+	public Robo() {
+		//this.nome = nome;
+	}
 
 	public Robo(Celula posicao, int tempoSegundos) {
+		this.nome = nome;
 		this.posicao = posicao;
 		this.tempoSegundos = tempoSegundos;
 	}
-	
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void getNome() {
+		return nome;
+	}
 	
 	
 	
